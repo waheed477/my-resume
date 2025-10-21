7cb4981682b7c0a1f3f16e82b00e279cc3632472
waheed477 <azlanch93@gmail.com>
2025-10-21 20:55:59 +0500

    fix: remove wouter router for GitHub Pages compatibility - use single page app

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,20 +1,9 @@
-import { Switch, Route } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { ThemeProvider } from "@/components/ThemeProvider";
 import Portfolio from "@/pages/Portfolio";
-import NotFound from "@/pages/not-found";
-
-function Router() {
-  return (
-    <Switch>
-      <Route path="/" component={Portfolio} />
-      <Route component={NotFound} />
-    </Switch>
-  );
-}
 
 function App() {
   return (
@@ -22,7 +11,7 @@ function App() {
       <ThemeProvider defaultTheme="dark">
         <TooltipProvider>
           <Toaster />
-          <Router />
+          <Portfolio />
         </TooltipProvider>
       </ThemeProvider>
     </QueryClientProvider>
